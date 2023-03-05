bd89de17c9e6d8a77c0e9003f6f28c7a73162ce4
btconroy <loc.webdeveloper@gmail.com>
2023-03-04 19:40:22 -0500

    updated UI

--- public/index.js
+++ public/index.js
@@ -1,8 +1,34 @@
 const btns = document.querySelectorAll('button');
-
+const addForm = document.querySelector('.option-form-add');
+const updateForm = document.querySelector('.option-form-update');
+const close = document.querySelectorAll('.close');
 
 btns.forEach(x => {
-    x.addEventListener('click', () => {
-        alert('It Works');
+    x.addEventListener('click', (event) => {
+        let button = event.target;
+        switch (button.innerText) {
+            case 'Add' :
+                addForm.style.transform = 'translate(-50%, -20%)';
+            break;
+            case 'Update' :
+                updateForm.style.transform = 'translate(-50%, -20%)';
+            break;
+            default: 
+            console.log('end')
+            break;
+            
+        }
     })
 });
+close.forEach(x => {
+    x.addEventListener('click', (event) => {
+        const button = event.target;
+        for(let index = 0; index < close.length; index++) {
+            if(addForm.hasAttribute('style') && button === close[0]) {
+                addForm.removeAttribute('style');
+            } else {
+                updateForm.removeAttribute('style');
+            }
+        }
+    })
+})
